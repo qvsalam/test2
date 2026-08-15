cc466af27a249f338722965b1bc5b115b74a5b5a
qvsalam <qvsalam@gmail.com>
2026-08-16 00:21:21 +0300

    Add MegaSource scraper adapter for CinemaBox

---
+++ megasource/scraper.py
@@ -0,0 +1,9 @@
+"""MegaSource adapter for CinemaBox."""
+
+TITLE = "CinemaBox Iraq"
+VERSION = "1.0.0"
+DESCRIPTION = "MegaSource adapter for the CinemaBox provider"
+
+
+def get_streams(media_type, media_id, config=None):
+    return []
